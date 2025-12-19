919b1c9dc3133701257885e02eeba262c30723c3
hyojin777 <54x9mp@gmail.com>
2025-12-19 18:35:54 +0900

    [Backend] - 서버 연결 및 Port 번호 변경 + servlet 호출 + Hello!

--- back-end/src/main/java/com/example/demo/BackEndApplication.java
+++ back-end/src/main/java/com/example/demo/BackEndApplication.java
@@ -2,8 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan // 어노테이션 추가해야 servlet 호출 가능
 public class BackEndApplication {
 
 	public static void main(String[] args) {
