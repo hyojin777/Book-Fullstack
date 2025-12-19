c87bf1afbbec85e5d0d9a8e685de681ec41b6b38
hyojin777 <54x9mp@gmail.com>
2025-12-19 14:36:53 +0900

    .gitignore 추가 및 npm 다운로드

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,13 +1,19 @@
+import { Route, Routes } from 'react-router';
 import './App.css';
+import Homepage from './components/pages/Homepage';
 // React에서는 태그 만들어서 사용
 //  -> 주의: compenent 이름은 대문자로 사용
 //  -> 구조: () => () : return 생략 가능
 //          () => {} : return 생략 불가능
 const App = () => {
   return (
-    <div className="App">
-      Hello React!!!
-    </div>
+    <>
+      <div style = {{height : '100vh'}}>
+        <Routes>
+          <Route path = '/' exact = {true} element = {<Homepage />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
